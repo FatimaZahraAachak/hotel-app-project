7a23506743263e2f0d0261723922860c1212c58c
FatimaZahraAachak <fatiaachak@gmail.com>
2025-11-28 15:09:00 +0100

    feat: add detailed mock hotel data with descriptions and amenities

--- src/data/mockHotels.ts
+++ src/data/mockHotels.ts
@@ -1,13 +1,62 @@
 export const Hotels = [
     {
-        id: 1,
-        name: ,
-        location: ,
-        price: ,
-        rating: ,
-        image: ,
-        description: string,
-        amenities: string[]
+        id: "1",
+        name: "Sea Breeze Resort",
+        location: "Nice, France",
+        price: 180,
+        rating: 4.6,
+        image: "https://images.unsplash.com/photo-1561501900-3701fa6a0864?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Un bel hôtel en bord de mer avec vue panoramique.",
+        amenities: ["Wi-Fi", "Piscine", "Petit déjeuner inclus"]
+    }
+    ,
+    {
+        id: "2",
+        name: "Hotel Sofitel Agadir Thalassa Sea & Spa",
+        location: "Agadir, Maroc",
+        price: 200,
+        rating: 4.7,
+        image: "https://images.getaroom-cdn.com/image/upload/s--f4NtQWQp--/c_limit,e_improve,fl_lossy.immutable_cache,h_940,q_auto:good,w_940/v1755817995/e9ca074653c739072e4df289636f9a73f7ac915c?_a=BACAEuDL&atc=e7cd1cfa",
+        description: "Un bel hôtel en bord de mer avec vue panoramique.",
+        amenities: ["Wi-Fi", "Piscine", "Petit déjeuner inclus", "SPA"]
     },
-    
+    {
+        id: "3",
+        name: "Royal Mansour Marrakech",
+        location: "Marrakech, Maroc",
+        price: 300,
+        rating: 5,
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/88033158.jpg?k=380e8f8fe3a857358f15cc485da1e17c7a572e6af46a42fa000c2a23525d68ad&o=",
+        description: "Doté de piscines extérieure et intérieure, le Royal Mansour Marrakech se trouve à Marrakech, à 2 km du palais de la Bahia et à 5 minutes de marche de la célèbre place Jemaa el-Fna.",
+        amenities: ["Wi-Fi", "Piscine", "Petit déjeuner inclus", "SPA"]
+    },
+    {
+        id: "4",
+        name: "Hotel Le Golf D'Essaouira & Spa",
+        location: "Essaouira, Maroc",
+        price: 250,
+        rating: 4.8,
+        image: "https://images.getaroom-cdn.com/image/upload/s--ofzOqSya--/c_limit,e_improve,fl_lossy.immutable_cache,h_940,q_auto:good,w_940/v1751657543/5b0199ca5e6d9cc16e430b558fd59e222f7b282b?_a=BACAEuDL&atc=e7cd1cfa",
+        description: "Un bel hôtel en bord de mer avec vue panoramique.",
+        amenities: ["Wi-Fi", "Piscine", "Petit déjeuner inclus", "SPA"]
+    }, {
+        id: "5",
+        name: "Milaidhoo Maldives",
+        location: "Maldives",
+        price: 4000,
+        rating: 5,
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/331619723.jpg?k=9f39aaaec26500323eab20fd4cdf829d61359c90580d51bf901d3de14da5da0c&o=",
+        description: "Le Milaidhoo Maldives est un complexe de luxe de caractère situé dans la réserve de biosphère de l'UNESCO, à proximité d'Hanifaru Bay.",
+        amenities: ["Wi-Fi", "Piscine", "Petit déjeuner inclus", "SPA"]
+    },
+    {
+        id: "6",
+        name: "Wyndham Istanbul Old City",
+        location: "Istanbul, Turkey",
+        price: 161,
+        rating: 4.5,
+        image: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/769522675.jpg?k=d9be3ad2ed78a622f8246ed4837a4ed417934ce1a2226b3d476ef6c4e6cffdaf&o=",
+        description: "Cet hôtel 5 étoiles assure des services d’étage et de concierge. Vous pourrez également profiter d’un spa et centre de bien-être comprenant une salle de sport, des piscines intérieure et extérieure, un sauna, un bain turc ainsi qu’une terrasse",
+        amenities: ["Wi-Fi", "Piscine", "Petit déjeuner inclus", "SPA"]
+    },
 ]
