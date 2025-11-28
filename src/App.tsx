821dfa507897822c096f01236e15b5bcb1bd66f8
FatimaZahraAachak <fatiaachak@gmail.com>
2025-11-28 11:40:42 +0100

    chore: integrate Tailwind CSS and PostCSS into the project
    - Added Tailwind CSS and PostCSS dependencies in package.json.
    - Created postcss.config.js to configure PostCSS with Tailwind CSS and Autoprefixer.
    - Removed App.css as styles are now managed by Tailwind CSS.
    - Updated App.tsx to remove the import of App.css.
    - Added Tailwind directives in index.css for base, components, and utilities.
    - Created tailwind.config.js to configure Tailwind CSS content paths.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router'
-import './App.css'
 import Home from './pages/Home'
 import HotelDetails from './pages/HotelDetails'
 import MyReservations from './pages/MyReservations'
