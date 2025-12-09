5765d061c018d54562a01e7d5310c29db6d572c4
FatimaZahraAachak <fatiaachak@gmail.com>
2025-12-09 09:26:29 +0100

    feat: enhance BookingModal styling for improved layout and responsiveness

--- src/components/BookingModal.tsx
+++ src/components/BookingModal.tsx
@@ -19,12 +19,12 @@ function BookingModal({ open, onClose, id, price }: BookingModalProps) {
             center
             classNames={{
                 overlay: "bg-black/50 backdrop-blur-sm",
-                modal: "bg-white rounded-none md:rounded-2xl shadow-xl p-6  transition-all w-screen h-screen md:w-auto md:h-auto",
+                modal: "bg-white rounded-none md:rounded-2xl shadow-xl p-6  transition-all w-screen h-screen md:w-auto  md:min-h-[400px] md:max-h-[55vh]",
                 closeButton: "absolute top-3 right-3 text-gray-500 hover:text-gray-700",
             }}
         >
-            <div className="flex flex-col gap-5 items-center     max-w-sm md:max-w-md lg:max-w-lg max-h-[85vh] overflow-y-auto  p-4 md:p-5 ">
-                <h3 className="font-bold">Réserver votre séjour</h3>
+            <div className="flex flex-col gap-5 items-center     max-w-sm md:max-w-md lg:max-w-lg md:h-full overflow-y-auto  p-4 md:p-5 ">
+                <h3 className="font-bold sticky top-0 bg-white z-10 pb-2">Réserver votre séjour</h3>
                 <BookingForm id={id} price={price} />
             </div>
         </Modal>
