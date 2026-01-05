5928ce340098fcaca78d43d5c1db27aaff6300a3
FatimaZahraAachak <fatiaachak@gmail.com>
2026-01-05 18:54:27 +0100

    feat: implement BookingProvider; manage hotel reservations with Supabase integration

--- src/context/BookingContext.tsx
+++ src/context/BookingContext.tsx
@@ -1,51 +1,8 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext } from "react";
 import type { NewReservation, Reservation } from "../types"
-import { supabase } from "../services/supabase";
-
 type BookingContextValue = {
     reservations: Reservation[],
     addReservation: (res: NewReservation) => Promise<void>,
     removeReservation: (resId: number) => Promise<void>
 }
 export const BookingContext = createContext<BookingContextValue | undefined>(undefined)
-export const BookingProvider = ({ children }: { children: React.ReactNode }) => {
-    const [reservations, setReservations] = useState<Reservation[]>([]);
-    useEffect(() => {
-        getReservation();
-    }, [])
-    async function getReservation() {
-        let { data } = await supabase
-            .from('reservations')
-            .select('*, hotel(*)');
-        if (!data) return;
-        console.log(data);
-        setReservations(data);
-    }
-    async function addReservation(reservation: NewReservation) {
-        await supabase
-            .from('reservations')
-            .insert([
-                reservation,
-            ]);
-
-        getReservation();
-    }
-    async function removeReservation(resId: number) {
-        const { error } = await supabase
-            .from('reservations')
-            .delete()
-            .eq('id', resId);
-        if (error) {
-            console.error(error);
-            return;
-        }
-        getReservation();
-
-    }
-    const value = {
-        reservations,
-        addReservation,
-        removeReservation
-    }
-    return <BookingContext.Provider value={value}>{children}</BookingContext.Provider>
-}
---
+++ src/context/BookingProvider.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../services/supabase";
+import type { NewReservation, Reservation } from "../types";
+import { BookingContext } from "./BookingContext";
+
+export const BookingProvider = ({ children }: { children: React.ReactNode }) => {
+    const [reservations, setReservations] = useState<Reservation[]>([]);
+    useEffect(() => {
+        async function getReservation() {
+            const { data } = await supabase
+                .from('reservations')
+                .select('*, hotel(*)');
+            if (!data) return;
+            console.log(data);
+            setReservations(data);
+        }
+        getReservation();
+    }, [])
+
+    async function addReservation(reservation: NewReservation) {
+        await supabase
+            .from('reservations')
+            .insert([
+                reservation,
+            ]);
+
+        const { data } = await supabase
+            .from("reservations")
+            .select("*, hotel(*)");
+
+        if (data) setReservations(data);
+    }
+    async function removeReservation(resId: number) {
+        const { error } = await supabase
+            .from('reservations')
+            .delete()
+            .eq('id', resId);
+        if (error) {
+            console.error(error);
+            return;
+        }
+        setReservations(prev =>
+            prev.filter(r => r.id !== resId)
+        );
+
+    }
+
+    return <BookingContext.Provider value={{
+        reservations,
+        addReservation,
+        removeReservation
+    }}>{children}</BookingContext.Provider>
+}
