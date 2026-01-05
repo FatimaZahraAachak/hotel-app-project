4e4c1470be560bcdcbc91a0a5b3dd03cd1615aa1
FatimaZahraAachak <fatiaachak@gmail.com>
2026-01-05 19:13:04 +0100

    refactor: update import statements for context providers in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import './index.css'
 import 'react-responsive-modal/styles.css';
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
-import { HotelProvider } from './context/HotelContext.tsx'
-import { BookingProvider } from './context/BookingContext.tsx'
-import { FavoriteProvider } from './context/FavoriteContext.tsx';
-import { SearchProvider } from './context/SearchContext.tsx';
+import { HotelProvider } from './context/HotelProvider.tsx';
+import { SearchProvider } from './context/SearchProvider.tsx';
+import { BookingProvider } from './context/BookingProvider.tsx';
+import { FavoriteProvider } from './context/FavoriteProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
