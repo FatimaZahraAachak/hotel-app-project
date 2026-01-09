05558c5386fa176e74f3b4bb0409766802c2e0c7
FatimaZahraAachak <fatiaachak@gmail.com>
2026-01-09 14:38:04 +0100

    style: enhance Login component layout and styling for improved user experience

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -44,15 +44,45 @@ function Login() {
 
 
     return (
-        <form onSubmit={handleSubmit} className="flex flex-col justify-center items-center gap-4 min-h-[500px] max-w-[400px] bg-white border rounded-2xl shadow p-6 ">
-            <div className="flex flex-col gap-2">
-                <h1 className="text-xl md:text-2xl text-blue-500 font-bold ">Connexion</h1>
-                <p className="text-xs text-gray-500 text-center">Bienvenue!Veuillez vous connecter à votre compte.</p>
-            </div>
-            <input type='email' className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-blue-300 placeholder:text-gray-400  " value={userName} onChange={handleUserName} placeholder="Adesse E-mail" />
-            <input type='password' className=" w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-200 focus:border-blue-200 placeholder:text-gray-400 " value={passWord} onChange={handlePassWord} placeholder="Mot de passe" />
-            <button type='submit' className='w-full mt-5  rounded-xl  px-4 py-2.5 text-sm font-medium text-white bg-blue-500 hover:bg-blue-700 ' >Se connecter</button>
-        </form>
-    )
+        <div className=" flex items-center  px-2 py-20">
+            <form
+                onSubmit={handleSubmit}
+                className="mx-auto flex flex-col gap-4 w-full max-w-[600px] bg-white border rounded-2xl shadow-lg p-10"
+            >
+                <div className="flex flex-col gap-2">
+                    <h1 className="text-xl md:text-2xl text-blue-500 font-bold text-center">
+                        Connexion
+                    </h1>
+                    <p className="text-xs text-gray-500 text-center">
+                        Bienvenue ! Veuillez vous connecter à votre compte.
+                    </p>
+                </div>
+
+                <input
+                    type="email"
+                    className="w-full border rounded-md px-4 py-3 focus:ring-2 focus:ring-blue-300"
+                    value={userName}
+                    onChange={handleUserName}
+                    placeholder="Adresse e-mail"
+                />
+
+                <input
+                    type="password"
+                    className="w-full border rounded-md px-4 py-3 focus:ring-2 focus:ring-blue-200"
+                    value={passWord}
+                    onChange={handlePassWord}
+                    placeholder="Mot de passe"
+                />
+
+                <button
+                    type="submit"
+                    className="w-full mt-5 rounded-xl px-4 py-3 text-sm font-medium text-white bg-blue-500 hover:bg-blue-700"
+                >
+                    Se connecter
+                </button>
+            </form>
+        </div>
+    );
+
 }
 export default Login;
