7f4edd5a981fbd6b5ceab7d4afa3d75997199c83
FatimaZahraAachak <fatiaachak@gmail.com>
2026-02-17 15:28:18 +0100

    Mock supabase globally for tests

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -4,7 +4,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY
-console.log(supabaseUrl, supabaseKey)
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
